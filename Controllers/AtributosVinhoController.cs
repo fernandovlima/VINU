25ecdf9c0bcab24800e065ff4eed2dc857e52580
fernandovlima <32498363+fernandovlima@users.noreply.github.com>
2018-06-20 19:24:56 -0300

    Add classes
    adicionando classes Controller para Vinho e AtributosVinho.

---
+++ Controllers/AtributosVinhoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Controllers.Base;
+using Controllers.DAL;
+using Models;
+
+namespace Controllers
+{
+	public class AtributosVinhoController : IBaseController<AtributosVinho>
+    {
+		private Context context = new Context();
+
+        public AtributosVinhoController()
+        {
+        }
+
+		public void AddNew(AtributosVinho entity)
+		{
+			context.Attributes.Add(entity);
+			context.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			AtributosVinho atributos = SearchById(id);
+
+			if(atributos != null)
+			{
+				context.Attributes.Remove(atributos);
+				context.SaveChanges();
+			}
+
+
+		}
+
+		public void Edit(AtributosVinho entity)
+		{
+			context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+		}
+
+		public ICollection<AtributosVinho> ListAll()
+		{
+			return context.Attributes.ToList();
+		}
+
+		public ICollection<AtributosVinho> ListByName(string name)
+		{
+			return context.Attributes.Where(AtributosVinho => AtributosVinho.NomeAtributo == name).ToList();
+		}
+
+		public AtributosVinho SearchById(int id)
+		{
+			return context.Attributes.Find(id);
+		}
+	}
+}
---
+++ Controllers/VinhoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Controllers.Base;
+using Controllers.DAL;
+using Models;
+
+namespace Controllers
+{
+	public class VinhoController : IBaseController<Vinho>
+    {
+		private Context context = new Context();
+
+        public VinhoController()
+        {
+        }
+
+		public void AddNew(Vinho entity)
+		{
+			context.Wines.Add(entity);
+			context.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			Vinho vinho = SearchById(id);
+
+			if( vinho != null)
+			{
+				context.Wines.Remove(vinho);
+				context.SaveChanges();
+
+			}
+		}
+
+		public void Edit(Vinho entity)
+		{
+			context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+			context.SaveChanges();
+
+		}
+
+		public ICollection<Vinho> ListAll()
+		{
+			return context.Wines.ToList();
+		}
+
+		public ICollection<Vinho> ListByName(string name)
+		{
+			return context.Wines.Where(vinho => vinho.NomeVinho == name).ToList();
+		}
+
+		public Vinho SearchById(int id)
+		{
+			return context.Wines.Find(id);
+		}
+	}
+}
